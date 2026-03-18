26c84471dd01e95bcae306469b46f5538d83a643
IsaelSousa <pajedeath@gmail.com>
2026-03-17 22:52:08 -0300

    feat: remove should notify

---
+++ app_manager_device_infrastructure/Migrations/20260318013914_AddShouldNotify.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace app_manager_device_infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShouldNotify : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShouldUpdate",
+                schema: "AppManager",
+                table: "Devices",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShouldUpdate",
+                schema: "AppManager",
+                table: "Apps",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShouldUpdate",
+                schema: "AppManager",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "ShouldUpdate",
+                schema: "AppManager",
+                table: "Apps");
+        }
+    }
+}
